1d640d96266fed6e86cc9d9a1f298687b7b96735
Math012 <math111.0811@gmail.com>
2025-02-06 14:59:14 -0300

    Criando a classe Manager.java

---
+++ src/utils/Manager.java
@@ -0,0 +1,34 @@
+package utils;
+
+public class Manager {
+
+    private String login;
+
+    private String password;
+
+    private String commission;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getCommission() {
+        return commission;
+    }
+
+    public void setCommission(String commission) {
+        this.commission = commission;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
