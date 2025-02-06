cccc28788e92a17bbc6b23fbb9a39bbfad8519e2
Math012 <math111.0811@gmail.com>
2025-02-06 15:53:49 -0300

    Utilizando formas de casting de um tipo especifico para um tipo mais generico

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,55 @@
 import utils.Employee;
 import utils.Manager;
+import utils.Salesman;
 
 public class Main {
     public static void main(String[] args) {
-        Employee employee = new Employee();
-        Manager manager = new Manager();
-
-        manager.setName("João");
-        manager.setLogin("joao");
-        manager.setPassword("12345");
-
-        System.out.println(manager.getName());
-        System.out.println(manager.getLogin());
-        System.out.println(manager.getPassword());
-
-        // convertendo um tipo especifico para um tipo mais generico
-
+        /*
+            convertendo um tipo especifico para um tipo mais generico.
+            Temos acesso a todas as propriedades do tipo generico (Employee) mas
+            não temos acesso ao tipo especifico (Manager)
+         */
         Employee managerGeneric = new Manager();
+        Employee salesmanGeneric = new Salesman();
+
+        // uma forma de casting mais explicita
+        printEmployee(managerGeneric);
+
+        // uma forma de casting sem a necessidade de implementação
+        printEmployeeFeatureJava17(managerGeneric);
+        printEmployeeFeatureJava17(salesmanGeneric);
+    }
 
 
+    static void printEmployee(Employee employee){
+        System.out.println("Class: " + employee.getClass().getCanonicalName());
+        employee.setName("João");
+        ((Manager)employee).setLogin("login Manager casting manual");
+        ((Manager)employee).setPassword("password Manager casting manual");
+        System.out.println(((Manager)employee).getLogin());
+        System.out.println(((Manager)employee).getPassword());
+    }
+
+    static void printEmployeeFeatureJava17(Employee employee){
+        // A verificação gera uma variavel da instancia, entrou no Java 17.
+        if (employee instanceof Manager manager){
+            System.out.println("=================================================");
+            System.out.println("Class: " + employee.getClass().getCanonicalName());
+            employee.setName("João");
+            manager.setLogin("login Manager");
+            manager.setPassword("password Manager");
+            System.out.println(manager.getLogin());
+            System.out.println(manager.getPassword());
+            System.out.println("=================================================");
+        }
+        if (employee instanceof Salesman salesman){
+            System.out.println("=================================================");
+            System.out.println("Class: " + employee.getClass().getCanonicalName());
+            employee.setName("Matheus Salesman");
+            salesman.setPercentPerSold(10.0);
+            System.out.println(employee.getName());
+            System.out.println(salesman.getPercentPerSold());
+            System.out.println("=================================================");
+        }
     }
 }
