4babe8165736b298d24207c3544a4ae640325d07
Math012 <math111.0811@gmail.com>
2025-02-06 15:00:08 -0300

    Criando a classe Salesman.java

---
+++ src/utils/Salesman.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class Salesman {
+
+    private Double percentPerSold;
+
+    public Double getPercentPerSold() {
+        return percentPerSold;
+    }
+
+    public void setPercentPerSold(Double percentPerSold) {
+        this.percentPerSold = percentPerSold;
+    }
+}
